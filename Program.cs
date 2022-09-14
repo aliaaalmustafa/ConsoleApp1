4ddccc5c18d67673d858adf2c605ac6220bc68ef
User <User@DESKTOP-FP4OP26>
2022-09-14 12:53:19 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+//Aufgabe 4
+
+Console.WriteLine("Bitte geben Sie die  erste Zahl ein ");
+Console.WriteLine("Bitte geben sie die zweite zahl ein ");
+Console.WriteLine(17 + 19);
+
